e77c5bd90280387652e85de03b4bb2de19186b03
Abdelrahman Ebied <abdelrahmanm.ebied@gmail.com>
2025-02-06 13:59:37 +0200

    chore: add project configuration files and dependencies

---
+++ jest.setup.js
@@ -0,0 +1,35 @@
+import 'react-native-gesture-handler/jestSetup';
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+  Reanimated.default.call = () => {};
+  return Reanimated;
+});
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    ...jest.requireActual('@react-navigation/native'),
+    useNavigation: () => ({
+      navigate: jest.fn(),
+      goBack: jest.fn(),
+    }),
+  };
+});
+
+// Mock environment variables
+jest.mock('@env', () => ({
+  WEATHER_API_KEY: 'test_api_key',
+}));
+
+// Mock geolocation
+jest.mock('react-native-geolocation-service', () => ({
+  getCurrentPosition: jest.fn(),
+}));
+
+// Silence the warning: Animated: `useNativeDriver` is not supported
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+// Mock vector icons
+jest.mock('react-native-vector-icons/Ionicons', () => 'Icon'); 
